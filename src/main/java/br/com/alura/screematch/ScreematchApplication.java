b7194bba1209575670df8844c19e4d819bac9c8b
Kummer1 <gustavo.kummer1@gmail.com>
2025-05-29 17:17:44 -0300

    first commit

---
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -0,0 +1,28 @@
+package br.com.alura.screematch;
+
+import model.DadosSerie;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import service.ConsumoAPI;
+import service.ConverteDados;
+
+@SpringBootApplication
+public class ScreematchApplication implements CommandLineRunner {
+
+	public static void main(String[] args){
+		SpringApplication.run(ScreematchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumoAPI = new ConsumoAPI();
+		var json =
+		consumoAPI.obterDados(
+				"http://www.omdbapi.com/?t=gilmore+girls&apikey=1c72e09e");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+	}
+}
---
+++ src/main/java/service/ConverteDados.java
@@ -0,0 +1,12 @@
+package service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class ConverteDados implements IConverteDados {
+    private ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return mapper.convertValue(json, classe);
+    }
+    }
+
