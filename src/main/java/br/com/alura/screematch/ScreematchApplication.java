77cc026b8d38ffadbe3853e06b603aff517db948
Gustavo Kummer <gustavo.kummer1@gmail.com>
2025-05-29 19:06:31 -0300

    Correcao ConverterDados

--- src/main/java/br/com/alura/screematch/ScreematchApplication.java
+++ src/main/java/br/com/alura/screematch/ScreematchApplication.java
@@ -20,9 +20,12 @@ public class ScreematchApplication implements CommandLineRunner {
 		var json =
 		consumoAPI.obterDados(
 				"http://www.omdbapi.com/?t=gilmore+girls&apikey=1c72e09e");
+
 		System.out.println(json);
+		System.out.println("Teste");
+
 		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		var i = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(i);
 	}
 }
--- src/main/java/service/ConverteDados.java
+++ src/main/java/service/ConverteDados.java
@@ -1,4 +1,5 @@
 package service;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ConverteDados implements IConverteDados {
@@ -6,7 +7,12 @@ public class ConverteDados implements IConverteDados {
 
     @Override
     public <T> T obterDados(String json, Class<T> classe) {
-        return mapper.convertValue(json, classe);
+        try {
+            return mapper.readValue(json, classe);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+            }
+        }
     }
-    }
+
 
